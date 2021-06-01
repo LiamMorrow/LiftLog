c73d1006981d071307c8d047cea6b4e15aa70096
Liam Morrow <liam@citrusad.com>
2021-06-01 20:45:18 +1000

    Show workout history?

--- SimpleGymTracker.WebUi/Pages/Index.razor.cs
+++ SimpleGymTracker.WebUi/Pages/Index.razor.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Fluxor;
 using Microsoft.AspNetCore.Components;
 using SimpleGymTracker.Lib.Models;
+using SimpleGymTracker.Lib.Store;
 using SimpleGymTracker.WebUi.Store.WorkoutSession;
 using SimpleGymTracker.WebUi.Store.WorkoutSettings;
 
@@ -16,7 +19,29 @@ namespace SimpleGymTracker.WebUi.Pages
     public IDispatcher Dispatcher { get; set; } = null!;
 
     [Inject]
+    public IProgressStore ProgressStore { get; set; } = null!;
+
+    [Inject]
     public NavigationManager NavigationManager { get; set; } = null!;
+
+    private readonly List<WorkoutDayDao> _workouts = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+      await base.OnInitializedAsync();
+      await foreach (var workoutDao in ProgressStore.GetAllWorkoutDaysAsync())
+      {
+        _workouts.Add(workoutDao);
+        this.StateHasChanged();
+      }
+    }
+
+    private void SelectSession(WorkoutDayDao day)
+    {
+      Dispatcher.Dispatch(new SelectPlanAction(day.Day.Plan));
+      Dispatcher.Dispatch(new SetWorkoutDayAction(day));
+      NavigationManager.NavigateTo("/session");
+    }
 
     private void SelectWorkoutPlan(WorkoutPlan plan)
     {
