35f666a75f2565942ebb750e3f4dd8c5f2a7c49b
Liam Morrow <ldmorrow@outlook.com>
2023-04-19 20:53:15 +1000

    Schedule notifications

--- LiftLog.WebUi/Services/WebNotificationService.cs
+++ LiftLog.WebUi/Services/WebNotificationService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Append.Blazor.Notifications;
 using LiftLog.Lib.Services;
 using INotificationService = LiftLog.Lib.Services.INotificationService;
@@ -8,37 +9,27 @@ public class WebNotificationService : INotificationService
 {
     private readonly Append.Blazor.Notifications.INotificationService _notificationService;
 
+    private readonly ConcurrentDictionary<NotificationHandle, CancellationTokenSource> _scheduledNotifications = new ();
+
     public WebNotificationService(Append.Blazor.Notifications.INotificationService notificationService)
     {
         _notificationService = notificationService;
     }
 
-    public async Task<NotificationHandle> SendNotificationAsync(string title, string message)
-    {
+
+    public async Task UpdateNotificationAsync(NotificationHandle handle, string title, string message)
+    { 
         if (!await _notificationService.IsSupportedByBrowserAsync())
         {
-            return new NotificationHandle(Guid.Empty);
+            return;
         }
         if (_notificationService.PermissionStatus != PermissionType.Granted)
         {
             if (await _notificationService.RequestPermissionAsync() != PermissionType.Granted)
             {
-                return new NotificationHandle(Guid.Empty);
+                return;
             }
         }
-
-        var handle = new NotificationHandle(Guid.NewGuid());
-        await _notificationService.CreateAsync(title, new NotificationOptions()
-        {
-            Tag = handle.Id.ToString(),
-            Body = message,
-            Renotify = false
-        });
-        return handle;
-    }
-
-    public async Task UpdateNotificationAsync(NotificationHandle handle, string title, string message)
-    {
         await _notificationService.CreateAsync(title, new NotificationOptions()
         {
             Tag = handle.Id.ToString(),
@@ -49,6 +40,39 @@ public class WebNotificationService : INotificationService
 
     public Task ClearNotificationAsync(NotificationHandle handle)
     {
+        if (_scheduledNotifications.TryGetValue(handle, out var cancellation))
+        {
+            cancellation.Cancel();
+        }
         return Task.CompletedTask;
+    }
+
+    public async Task ScheduleNotificationAsync(NotificationHandle handle, DateTimeOffset scheduledFor, string title,
+        string message)
+    {
+        if (!await _notificationService.IsSupportedByBrowserAsync())
+        {
+            return;
+        }
+        if (_notificationService.PermissionStatus != PermissionType.Granted)
+        {
+            if (await _notificationService.RequestPermissionAsync() != PermissionType.Granted)
+            {
+                return;
+            }
+        }
+        await ClearNotificationAsync(handle);
+        var source = new CancellationTokenSource();
+        if (_scheduledNotifications.TryAdd(handle, source))
+        {
+            var timeToWait = new[] { scheduledFor - DateTimeOffset.Now, TimeSpan.Zero }.Max();
+            _ = Task.Delay(timeToWait, source.Token).ContinueWith((result) =>
+            {
+                if (result.IsCompletedSuccessfully)
+                {
+                    _ = UpdateNotificationAsync(handle, title, message);
+                }
+            });
+        }
     }
 }
