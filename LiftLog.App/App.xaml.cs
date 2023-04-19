4d6145aea74c4031b3255c3db5385a3146c3fd5f
Liam Morrow <ldmorrow@outlook.com>
2023-04-19 22:24:04 +1000

    Add maui app

---
+++ LiftLog.App/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace LiftLog.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "LiftLog.App";
+            return window;
+        }
+    }
+}
---
+++ LiftLog.App/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace LiftLog.App.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Current count: {count}";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+    }
+}
