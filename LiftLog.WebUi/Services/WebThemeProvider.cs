a97ce6721c6eb2fac1e863b06e3b2dfc919b01c7
Liam Morrow <ldmorrow@outlook.com>
2023-05-15 20:43:25 +1000

    Support dark theme on web

--- LiftLog.WebUi/Services/WebThemeProvider.cs
+++ LiftLog.WebUi/Services/WebThemeProvider.cs
@@ -1,6 +1,9 @@
 using LiftLog.Ui.Services;
 using MaterialColorUtilities.Palettes;
 using MaterialColorUtilities.Schemes;
+using Microsoft.JSInterop;
+using RealGoodApps.BlazorJavascript.Interop;
+using RealGoodApps.BlazorJavascript.Interop.Extensions;
 
 namespace LiftLog.WebUi.Services;
 
@@ -9,11 +12,23 @@ public class WebThemeProvider : IThemeProvider
     private readonly CorePalette _corePalette;
     private readonly Scheme<uint> _scheme;
 
-    public WebThemeProvider()
+    public WebThemeProvider(IJSRuntime jsRuntime)
     {
         _corePalette = new();
         _corePalette.Fill(0xF44336);
-        _scheme = new LightSchemeMapper().Map(_corePalette);
+
+
+        if (jsRuntime is not IJSInProcessRuntime jsInProcessRuntime)
+        {
+            throw new InvalidCastException("The JS runtime must be in-process.");
+        }
+
+        BlazorJavascriptInitialization.Initialize(jsInProcessRuntime);
+        var window = jsInProcessRuntime.GetWindow()!;
+        _scheme = window.matchMedia(jsInProcessRuntime.CreateString("(prefers-color-scheme: dark)")).matches
+            .ConvertToValue<bool>()
+            ? new DarkSchemeMapper().Map(_corePalette)
+            : new LightSchemeMapper().Map(_corePalette);
     }
 
     public Scheme<uint> GetColorScheme() => _scheme;
