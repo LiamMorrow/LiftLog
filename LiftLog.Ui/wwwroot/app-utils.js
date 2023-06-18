777477ad97d2474e1b8dbec8849eaa3580d8d4e3
Liam Morrow <ldmorrow@outlook.com>
2023-06-18 11:18:28 +1000

    Add ai walkthrough skeleton

--- LiftLog.Ui/wwwroot/app-utils.js
+++ LiftLog.Ui/wwwroot/app-utils.js
@@ -2,7 +2,27 @@ var AppUtils = {};
 AppUtils.getScrollTop = function (element) {
     return element.scrollTop
 };
+/**
+ *
+ * @param {HTMLElement} element
+ */
+AppUtils.getMiddleElementAttribute = function (element, attribute) {
+    const absolutePositionOfElement = element.getBoundingClientRect();
+    const middle = absolutePositionOfElement.top + (absolutePositionOfElement.height / 2);
+    const middleX = absolutePositionOfElement.left + (absolutePositionOfElement.width / 2);
+    const elementAtMiddle = document.elementFromPoint(middleX, middle);
+    return elementAtMiddle?.getAttribute(attribute);
+}/**
+ *
+ * @param {HTMLElement} element
+ */
+AppUtils.setMiddleElementAttribute = function (element, attribute, value) {
+    const absolutePositionOfElement = element.getBoundingClientRect();
+    const middle = absolutePositionOfElement.top + (absolutePositionOfElement.height / 2);
+    const middleX = absolutePositionOfElement.left + (absolutePositionOfElement.width / 2);
+    const elementAtMiddle = document.elementFromPoint(middleX, middle);
+    elementAtMiddle?.setAttribute(attribute, value);
+}
 
-AppUtils.createRipple = function (element, event)
-{
+AppUtils.createRipple = function (element, event) {
 }
