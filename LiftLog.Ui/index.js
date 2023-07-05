7d8d1a5189da7170a6400c6dd288f61c3e564048
Liam Morrow <ldmorrow@outlook.com>
2023-07-05 21:35:15 +1000

    Restyled all icon buttons to match m3

--- LiftLog.Ui/index.js
+++ LiftLog.Ui/index.js
@@ -1,5 +1,10 @@
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
+import '@material/web/button/elevated-button.js';
 import '@material/web/button/tonal-button.js';
 import '@material/web/focus/focus-ring.js';
 import '@material/web/ripple/ripple.js';
+import '@material/web/icon/icon.js';
+import '@material/web/iconbutton/filled-icon-button.js';
+import '@material/web/iconbutton/outlined-icon-button.js';
+import '@material/web/iconbutton/filled-tonal-icon-button.js';
