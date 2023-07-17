ab9d6156fdb9df341d0a09d10dc99985da52e9ec
Liam Morrow <ldmorrow@outlook.com>
2023-07-17 21:05:50 +1000

    When the app resumes, reset the theme

--- LiftLog.App/App.xaml.cs
+++ LiftLog.App/App.xaml.cs
@@ -7,7 +7,24 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        IMaterialColorService.Current.Initialize(this.Resources);
         MainPage = new MainPage();
+    }
+
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Created += (s, e) =>
+        {
+             IMaterialColorService.Current.Initialize(Resources);
+        };
+
+        window.Resumed += (sender, args) =>
+        {
+            IMaterialColorService.Current.Initialize(Resources);
+        };
+
+        return window;
     }
 }
