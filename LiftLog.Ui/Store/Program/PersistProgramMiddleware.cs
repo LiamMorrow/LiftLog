2043ab810f3e3983536cb20924a1ba531303c49d
Liam Morrow <ldmorrow@outlook.com>
2023-08-14 18:23:21 +1000

    Speed

--- LiftLog.Ui/Store/Program/PersistProgramMiddleware.cs
+++ LiftLog.Ui/Store/Program/PersistProgramMiddleware.cs
@@ -30,6 +30,7 @@ public class PersistProgramMiddleware : Middleware
             return;
         }
 
+
         _programRepository.PersistSessionsInProgramAsync(currentState.SessionBlueprints);
     }
 }
