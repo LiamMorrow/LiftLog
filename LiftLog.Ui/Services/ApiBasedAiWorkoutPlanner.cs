6f7f2d2677592fcb0c1048adfc705fb1914994b8
Liam Morrow <ldmorrow@outlook.com>
2023-08-17 22:06:55 +1000

    Escape hatch for ai planner

--- LiftLog.Ui/Services/ApiBasedAiWorkoutPlanner.cs
+++ LiftLog.Ui/Services/ApiBasedAiWorkoutPlanner.cs
@@ -1,25 +1,36 @@
 using System.Text;
 using System.Text.Json;
+using Fluxor;
 using LiftLog.Lib.Models;
 using LiftLog.Lib.Serialization;
 using LiftLog.Lib.Services;
+using LiftLog.Ui.Store.App;
 
 namespace LiftLog.Ui.Services;
 
 public class ApiBasedAiWorkoutPlanner : IAiWorkoutPlanner
 {
     private readonly HttpClient httpClient;
+    private readonly IState<AppState> appState;
 
-    public ApiBasedAiWorkoutPlanner(HttpClient httpClient)
+    public ApiBasedAiWorkoutPlanner(HttpClient httpClient, IState<AppState> appState)
     {
         this.httpClient = httpClient;
+        this.appState = appState;
     }
     public async Task<AiWorkoutPlan> GenerateWorkoutPlanAsync(AiWorkoutAttributes attributes)
     {
-        var response = await httpClient.PostAsync(
-            "https://liftlog.azurewebsites.net/api/GenerateAiWorkout?",
-            new StringContent(JsonSerializer.Serialize(new GenerateAiWorkoutPlanRequest(attributes), JsonSerializerSettings.LiftLog), Encoding.UTF8, "application/json"));
+        var proToken = appState.Value.ProState.ProToken;
+        if (proToken is null)
+        {
+            throw new Exception("Please upgrade to pro to use this feature.");
+        }
 
+        var request = new HttpRequestMessage(HttpMethod.Post, "https://liftlog.azurewebsites.net/api/GenerateAiWorkout?");
+        request.Headers.Add("Authorization", $"{proToken}");
+        request.Content = new StringContent(JsonSerializer.Serialize(new GenerateAiWorkoutPlanRequest(attributes), JsonSerializerSettings.LiftLog), Encoding.UTF8, "application/json");
+
+        var response = await httpClient.SendAsync(request);
         if (!response.IsSuccessStatusCode)
         {
             throw new Exception("Failed to generate workout plan.");
