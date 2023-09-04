73019b300d37d3271fc1fbb7d9dc6e27798bcfd6
Liam Morrow <ldmorrow@outlook.com>
2023-09-04 21:22:32 +1000

    Make debug build faster

--- LiftLog.App/install.sh
+++ LiftLog.App/install.sh
@@ -4,4 +4,11 @@ cd ../LiftLog.Ui
 yarn build
 cd ../LiftLog.App
 
+# If -r we want release build
+if [ "$1" == "-r" ]; then
+    echo "Release build"
 dotnet build -t:Run -c Release -p:EnableLLVM=false -p:AndroidEnableProfiledAot=true -p:ExtraDefineConstants=TEST_MODE #-p:AndroidEnableProfiler=true
+    exit 0
+fi
+
+dotnet build -t:Run -c Debug -p:ExtraDefineConstants=TEST_MODE #-p:AndroidEnableProfiler=true
