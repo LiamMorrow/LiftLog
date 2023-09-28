07c3625015d1ca204d2170acd29272e0651308ef
Li <ldmorrow@outlook.com>
2023-09-29 07:52:15 +1000

    1.6.1 - fix issue with selecting areas to workout

--- LiftLog.Ui/Store/App/AppFeature.cs
+++ LiftLog.Ui/Store/App/AppFeature.cs
@@ -12,7 +12,13 @@ public class AppFeature : Feature<AppState>
     protected override AppState GetInitialState() =>
         new(
             Title: "LiftLog",
-            ProState: new ProState(null),
+            ProState: new ProState(
+#if TEST_MODE
+                ProToken: "102bc25a-f46b-4423-9149-b0fa39b32f1e"
+#else
+                ProToken: null
+#endif
+            ),
             ReopenCurrentSession: true,
             BackNavigationUrl: null
         );
