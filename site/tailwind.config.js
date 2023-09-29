774ecd171ae0291f3e4bbb5878a1d9af49e29759
Li <ldmorrow@outlook.com>
2023-09-29 20:53:30 +1000

    Add static site

---
+++ site/tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
