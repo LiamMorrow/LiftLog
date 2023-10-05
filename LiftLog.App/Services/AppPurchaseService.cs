86f4f1fcc2e5c5bdb10b61bd1001cc702ad1d109
Li <ldmorrow@outlook.com>
2023-10-05 20:04:01 +1000

    Remove IOS free workout generator

--- LiftLog.App/Services/AppPurchaseService.cs
+++ LiftLog.App/Services/AppPurchaseService.cs
@@ -17,17 +17,21 @@ public class AppPurchaseService : IAppPurchaseService
 
     public AppStore GetAppStore()
     {
+#if TEST_MODE
+        return AppStore.Web;
+#else
         return DeviceInfo.Platform switch
         {
             var e when e == DevicePlatform.Android => AppStore.Google,
-            var e when e == DevicePlatform.iOS => AppStore.Web,
+            var e when e == DevicePlatform.iOS => AppStore.Apple,
             _ => AppStore.Web
         };
+#endif
     }
 
     public async Task<string?> GetProKeyAsync()
     {
-#if IOS
+#if TEST_MODE
         if (GetAppStore() == AppStore.Web)
         {
             return "102bc25a-f46b-4423-9149-b0fa39b32f1e";
