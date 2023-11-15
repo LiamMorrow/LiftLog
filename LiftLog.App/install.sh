91c2cb66aac62b80d5da269c3a609ab1edc3bb77
Li <ldmorrow@outlook.com>
2023-11-15 19:05:29 +1000

    Update to 8.0

--- LiftLog.App/install.sh
+++ LiftLog.App/install.sh
@@ -7,8 +7,8 @@ cd ../LiftLog.App
 # If -r we want release build
 if [ "$1" == "-r" ]; then
     echo "Release build"
-dotnet build -t:Run -c Release -f net7.0-android  -p:EnableLLVM=false -p:AndroidEnableProfiledAot=true -p:ExtraDefineConstants=TEST_MODE #-p:AndroidEnableProfiler=true
+dotnet build -t:Run -c Release -f net8.0-android  -p:EnableLLVM=false -p:AndroidEnableProfiledAot=true -p:ExtraDefineConstants=TEST_MODE #-p:AndroidEnableProfiler=true
     exit 0
 fi
 
-dotnet build -t:Run -c Debug -f net7.0-android -p:ExtraDefineConstants=TEST_MODE #-p:AndroidEnableProfiler=true
+dotnet build -t:Run -c Debug -f net8.0-android -p:ExtraDefineConstants=TEST_MODE #-p:AndroidEnableProfiler=true
