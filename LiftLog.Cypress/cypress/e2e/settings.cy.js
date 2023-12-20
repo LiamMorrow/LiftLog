5c66328414b8f6c27a465f0f5616370ea398362f
Li <ldmorrow@outlook.com>
2023-12-20 17:34:42 +1000

    Add cypress tests

--- LiftLog.Cypress/cypress/e2e/settings.cy.js
+++ LiftLog.Cypress/cypress/e2e/settings.cy.js
@@ -16,13 +16,30 @@ describe('Settings', () => {
       it('should display weights in the correct units on all pages', () => {
         assertCorrectWeightUnitsOnAllPages('kg')
         cy.get('nav').contains('Settings').click()
-        cy.contains('Use Imperial Units').click()
+        cy.contains('Use imperial units').click()
         assertCorrectWeightUnitsOnAllPages('lbs')
+      })
+    })
+
+    describe('and updates the bodyweight setting', () => {
+      it('should hide and show it on all pages', () => {
+        assertShowsBodyweightOnAllPages(true)
+        cy.get('nav').contains('Settings').click()
+        cy.contains('Show bodyweight').click()
+        assertShowsBodyweightOnAllPages(false)
       })
     })
   })
 })
 
+function assertShowsBodyweightOnAllPages(shouldShow) {
+  const classify = shouldShow ? 'contain.text' : 'not.contain.text'
+  cy.get('nav').contains('Stats').click()
+  cy.get('.cardlist .card').first().should(classify, 'Bodyweight')
+  cy.get('nav').contains('Workout').click()
+  cy.get('.cardlist .card').first().click()
+  cy.get('.card').last().should(classify, 'Bodyweight')
+}
 
 function assertCorrectWeightUnitsOnAllPages(units) {
   cy.get('nav').contains('History').click()
