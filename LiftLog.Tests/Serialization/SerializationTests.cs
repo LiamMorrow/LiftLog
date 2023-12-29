275ca9270d8f72a6012960b24b907b298e52c138
Li <ldmorrow@outlook.com>
2023-12-29 21:45:27 +1000

    Test

--- LiftLog.Tests/Serialization/SerializationTests.cs
+++ LiftLog.Tests/Serialization/SerializationTests.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using FluentAssertions;
+using Google.Protobuf;
 using LiftLog.Lib.Models;
 using LiftLog.Ui.Models.SessionHistoryDao;
 
@@ -28,6 +29,26 @@ public class SerializationTests
             serialized,
             StorageJsonContext.Context.SessionHistoryDaoV1
         );
+
+        deserialized!.Should().Be(sessionHistoryDao);
+        deserialized!.ToModel().CompletedSessions.First().Value.Should().Be(session);
+    }
+
+    [Fact]
+    public void SessionHistoryDaoV2_SerializeDeserialize_RoundTrip()
+    {
+        var session = Sessions.CreateSession();
+        var sessionHistoryDao = SessionHistoryDaoV2.FromModel(
+            new SessionHistoryDaoContainer(
+                CompletedSessions: new Dictionary<Guid, Session>
+                {
+                    { session.Id, session }
+                }.ToImmutableDictionary()
+            )
+        );
+
+        var serialized = sessionHistoryDao.ToByteArray();
+        var deserialized = SessionHistoryDaoV2.Parser.ParseFrom(serialized);
 
         deserialized!.Should().Be(sessionHistoryDao);
         deserialized!.ToModel().CompletedSessions.First().Value.Should().Be(session);
