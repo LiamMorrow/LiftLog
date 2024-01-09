78a62512f41fabc77b654192cbb4c34561b7c7bc
Li <ldmorrow@outlook.com>
2024-01-09 19:08:04 +1000

    Pull inbox fetching into separate file

---
+++ LiftLog.Ui/Store/Feed/FeedEffects.Inbox.cs
@@ -0,0 +1,104 @@
+using System.Collections.Immutable;
+using Fluxor;
+using Google.Protobuf;
+using LiftLog.Lib;
+using LiftLog.Lib.Models;
+using LiftLog.Lib.Services;
+using LiftLog.Ui.Models;
+using LiftLog.Ui.Models.SessionHistoryDao;
+using LiftLog.Ui.Util;
+using Microsoft.Extensions.Logging;
+using static LiftLog.Ui.Models.UserEventPayload;
+
+namespace LiftLog.Ui.Store.Feed;
+
+public partial class FeedEffects
+{
+    [EffectMethod]
+    public async Task HandleFetchInboxItemsAction(FetchInboxItemsAction _, IDispatcher dispatcher)
+    {
+        var identity = state.Value.Identity;
+        if (identity is null)
+        {
+            return;
+        }
+        var inboxItemsResponse = await feedApiService.GetInboxMessagesAsync(
+            new GetInboxMessagesRequest(identity.Id, identity.Password)
+        );
+        if (!inboxItemsResponse.IsSuccess)
+        {
+            // TODO handle properly
+            return;
+        }
+
+        var encryptedInboxItems = inboxItemsResponse.Data.InboxMessages;
+        var inboxItems = (
+            await Task.WhenAll(
+                encryptedInboxItems.Select(x => DecryptIfValid(x, identity.RsaKeyPair.PrivateKey))
+            )
+        ).WhereNotNull();
+
+        ImmutableListValue<FollowRequest> newFollowRequests = inboxItems
+            .Where(
+                x => x.MessagePayloadCase == InboxMessageDao.MessagePayloadOneofCase.FollowRequest
+            )
+            .Select(
+                x =>
+                    new FollowRequest(
+                        UserId: x.FromUserId,
+                        Name: x.FollowRequest.Name?.ToString() ?? "Anonymous User",
+                        ProfilePicture: x.FollowRequest.ProfilePicture.IsEmpty
+                            ? null
+                            : x.FollowRequest.ProfilePicture.ToByteArray(),
+                        PublicKey: new RsaPublicKey(x.FollowRequest.PublicKey.ToByteArray())
+                    )
+            )
+            .ToImmutableList();
+        dispatcher.Dispatch(new AppendNewFollowRequestsAction(newFollowRequests));
+
+        var newFollowResponses = inboxItems
+            .Where(
+                x => x.MessagePayloadCase == InboxMessageDao.MessagePayloadOneofCase.FollowResponse
+            )
+            .Select(
+                x =>
+                    new FollowResponse(
+                        UserId: x.FromUserId,
+                        Accepted: x.FollowResponse.ResponsePayloadCase
+                            == FollowResponseDao.ResponsePayloadOneofCase.Accepted,
+                        AesKey: x.FollowResponse.ResponsePayloadCase
+                        == FollowResponseDao.ResponsePayloadOneofCase.Accepted
+                            ? new AesKey(x.FollowResponse.Accepted.AesKey.ToByteArray())
+                            : null,
+                        FollowSecret: x.FollowResponse.ResponsePayloadCase
+                        == FollowResponseDao.ResponsePayloadOneofCase.Accepted
+                            ? x.FollowResponse.Accepted.FollowSecret
+                            : null
+                    )
+            )
+            .ToImmutableList();
+
+        dispatcher.Dispatch(new ProcessFollowResponsesAction(newFollowResponses));
+
+        var followers = state.Value.Followers;
+        var unfollowNotifications = inboxItems
+            .Where(
+                x =>
+                    x.MessagePayloadCase
+                    == InboxMessageDao.MessagePayloadOneofCase.UnfollowNotification
+            )
+            .SelectMany(
+                x =>
+                    followers.Values.Where(
+                        f =>
+                            f.Id == x.FromUserId
+                            && f.FollowSecret == x.UnfollowNotification.FollowSecret
+                    )
+            );
+
+        foreach (var unfollowNotification in unfollowNotifications)
+        {
+            dispatcher.Dispatch(new RemoveFollowerAction(unfollowNotification));
+        }
+    }
+}
