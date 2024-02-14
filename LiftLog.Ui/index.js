0f8581c211f2bf980d4b4e7c80e60159404c074f
Li <ldmorrow@outlook.com>
2024-02-14 21:37:55 +1000

    Restyle the exercise editor
    The new design uses cards to group content, and cleans up rests dramatically.

--- LiftLog.Ui/index.js
+++ LiftLog.Ui/index.js
@@ -1,6 +1,8 @@
 import '@material/web/all.js';
 
 import '@material/web/chips/chip-set.js';
+import '@material/web/labs/segmentedbutton/outlined-segmented-button.js';
+import '@material/web/labs/segmentedbuttonset/outlined-segmented-button-set.js';
 
 import 'element-internals-polyfill';
 
