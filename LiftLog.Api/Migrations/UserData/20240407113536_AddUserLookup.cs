0e6c82e4dea87a26573a77ca015bed54308833f8
Li <ldmorrow@outlook.com>
2024-04-10 17:31:52 +1000

    Format

--- LiftLog.Api/Migrations/UserData/20240407113536_AddUserLookup.cs
+++ LiftLog.Api/Migrations/UserData/20240407113536_AddUserLookup.cs
@@ -10,36 +10,32 @@ namespace LiftLog.Api.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateSequence<int>(
-                name: "user_lookup_sequence");
+            migrationBuilder.CreateSequence<int>(name: "user_lookup_sequence");
 
             migrationBuilder.AddColumn<int>(
                 name: "user_lookup",
                 table: "users",
                 type: "integer",
                 nullable: false,
-                defaultValueSql: "nextval('user_lookup_sequence')");
+                defaultValueSql: "nextval('user_lookup_sequence')"
+            );
 
             migrationBuilder.CreateIndex(
                 name: "ix_users_user_lookup",
                 table: "users",
                 column: "user_lookup",
-                unique: true);
+                unique: true
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "ix_users_user_lookup",
-                table: "users");
+            migrationBuilder.DropIndex(name: "ix_users_user_lookup", table: "users");
 
-            migrationBuilder.DropColumn(
-                name: "user_lookup",
-                table: "users");
+            migrationBuilder.DropColumn(name: "user_lookup", table: "users");
 
-            migrationBuilder.DropSequence(
-                name: "user_lookup_sequence");
+            migrationBuilder.DropSequence(name: "user_lookup_sequence");
         }
     }
 }
