a28a135cac2e2e5e981461c4312ae83ff9a0ee07
Li <ldmorrow@outlook.com>
2024-07-28 22:48:16 +1000

    Use new name

--- tests/LiftLog.Tests.App/GlobalUsings.cs
+++ tests/LiftLog.Tests.App/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using System.Collections.Immutable;
-global using Detest;
-global using static Detest.TestBuilder;
 global using FluentAssertions;
 global using NSubstitute;
+global using Oatmilk;
+global using static Oatmilk.TestBuilder;
+global using Oatmilk.Xunit;
 global using Xunit;
