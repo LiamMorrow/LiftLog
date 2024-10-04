1ee2e8f1e7da864fd4a6293f7de8734c43851dd0
Li <ldmorrow@outlook.com>
2024-10-05 09:35:15 +1000

    Remove logs

--- LiftLog.Ui/wwwroot/app-utils.ts
+++ LiftLog.Ui/wwwroot/app-utils.ts
@@ -26,7 +26,6 @@ AppUtils.extractLinkFromClipboard = async function (): Promise<string | null> {
   const text: Blob | null = clipboard[0].types.includes("text/plain") ? await clipboard[0].getType("text/plain") : null;
   if (text) {
     const textString = await new Response(text).text();
-    console.log(textString);
     if (textString.startsWith("http")) {
       return textString;
     }
@@ -38,7 +37,6 @@ AppUtils.extractLinkFromClipboard = async function (): Promise<string | null> {
     const parser = new DOMParser();
     const doc = parser.parseFromString(htmlString, "text/html");
     const link = doc.querySelector("a");
-    console.log(htmlString);
     if (link) {
       return link.href;
     }
