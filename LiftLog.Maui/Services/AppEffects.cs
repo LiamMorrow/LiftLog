e554a3cffd2b628536d1a44d69a6c4eac8656f19
Liam Morrow <ldmorrow@outlook.com>
2024-11-27 18:33:07 +1100

    Revert to navigate action on android

--- LiftLog.Maui/Services/AppEffects.cs
+++ LiftLog.Maui/Services/AppEffects.cs
@@ -9,9 +9,16 @@ public class AppEffects()
     [EffectMethod]
     public async Task HandleOpenExternalUrlAction(
         OpenExternalUrlAction action,
-        Fluxor.IDispatcher _
+        Fluxor.IDispatcher dispatcher
     )
     {
+#if IOS
         await Browser.Default.OpenAsync(action.Url, BrowserLaunchMode.External);
+#elif ANDROID
+        // For whatever reason, adding queries to the manifest did not work on real devices, however we already have the capability to open external URLs
+        // with the NavigateAction
+        dispatcher.Dispatch(new NavigateAction(action.Url));
+        await Task.CompletedTask;
+#endif
     }
 }
