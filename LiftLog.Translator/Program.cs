ff94cba7b08bfc4cfe041ce113018ab8584a77e6
Liam Morrow <ldmorrow@outlook.com>
2024-12-09 22:06:21 +1100

    Add notes on how to translate

--- LiftLog.Translator/Program.cs
+++ LiftLog.Translator/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using LiftLog.Translator;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -10,5 +11,7 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
 });
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
