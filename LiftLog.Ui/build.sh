7e687db65dc8f8cb34a96f3710eb5de37fa7272a
Liam Morrow <ldmorrow@outlook.com>
2025-01-29 12:00:27 +1100

    Update tailwind to v4

--- LiftLog.Ui/build.sh
+++ LiftLog.Ui/build.sh
@@ -1,2 +1,2 @@
-tailwindcss --config tailwind.config.js -i ./Styles/app.css -o ./wwwroot/app.min.css
+npx @tailwindcss/cli --config tailwind.config.js -i ./Styles/app.css -o ./wwwroot/app.min.css
 rollup -p @rollup/plugin-node-resolve index.js --format iife > ./wwwroot/bundle.js
