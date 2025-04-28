3317ddc09aa7bfbc08cf26b3ef93b8ae0db2b4da
Liam Morrow <ldmorrow@outlook.com>
2025-04-28 19:32:24 +1000

    Comment

--- LiftLog.Maui/Services/FilePreferenceStore.cs
+++ LiftLog.Maui/Services/FilePreferenceStore.cs
@@ -2,6 +2,7 @@ using LiftLog.Ui.Services;
 
 namespace LiftLog.Maui.Services;
 
+// We are migrating to use file storage for all our store values for a while
 public class FilePreferenceStore(IKeyValueStore keyValueStore) : IPreferenceStore
 {
     public async ValueTask<string?> GetItemAsync(string key)
@@ -9,7 +10,12 @@ public class FilePreferenceStore(IKeyValueStore keyValueStore) : IPreferenceStor
         var fileValue = await keyValueStore.GetItemAsync(key);
         if (fileValue is null)
         {
-            return await SecureStorage.Default.GetAsync(key);
+            var oldVal = await SecureStorage.Default.GetAsync(key);
+            if (oldVal is not null)
+            {
+                await SetItemAsync(key, oldVal);
+            }
+            return oldVal;
         }
         return fileValue;
     }
