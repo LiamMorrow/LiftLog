97f9d5421b5bf898b593d89b1f63d97cddf15fb2
Liam Morrow <ldmorrow@outlook.com>
2025-05-10 17:50:47 +1000

    Fix switch animations

--- liftlog-react/components/presentation/list-switch.tsx
+++ liftlog-react/components/presentation/list-switch.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import { Platform } from 'react-native';
 import { List, Switch } from 'react-native-paper';
 
 interface ListSwitchProps {
@@ -7,16 +8,32 @@ interface ListSwitchProps {
   value: boolean;
   onValueChange: (value: boolean) => void;
 }
+function useDelay() {
+  if (Platform.OS !== 'android') {
+    return true;
+  }
+  const [gate, setGate] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setGate(true);
+    }, 10);
+  });
+  return gate;
+}
 
 export default function ListSwitch(props: ListSwitchProps) {
+  const delayRender = useDelay(); // Workaround - mount the switch after initial render. See: https://github.com/react-navigation/react-navigation/issues/8658#issuecomment-898486182
+
   return (
     <List.Item
       title={props.headline}
       description={props.supportingText}
       onPress={() => props.onValueChange(!props.value)}
-      right={() => (
-        <Switch value={props.value} onValueChange={props.onValueChange} />
-      )}
+      right={() =>
+        delayRender && (
+          <Switch value={props.value} onValueChange={props.onValueChange} />
+        )
+      }
     />
   );
 }
