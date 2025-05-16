cb28565540f2bc6b2a51723b64c8abd5c37fdc02
Jake Mc <jake.mc@icloud.com>
2025-05-16 21:48:49 +1000

    Changes icon imports to full path which stops android boot crash

--- liftlog-react/components/presentation/ms-icon-source.tsx
+++ liftlog-react/components/presentation/ms-icon-source.tsx
@@ -1,63 +1,68 @@
-import {
-  msAdd,
-  msArrowDownward,
-  msArrowUpward,
-  msAssignment,
-  msBugReport,
-  msClose,
-  msContentCopy,
-  msDelete,
-  msEdit,
-  msFitnessCenter,
-  msHistory,
-  msInfo,
-  msMoreHoriz,
-  msNotes,
-  msNotifications,
-  msRemove,
-  msSave,
-  msSettings,
-  msSettingsBackupRestore,
-  msShare,
-  msStar,
-  msTextAd,
-  msTranslate,
-  msWeight,
-} from '@material-symbols-react-native/outlined-400'; // Using the default weight (400)
-
+import { msAdd } from '@material-symbols-react-native/outlined-400/msAdd';
+import { msArrowDownward } from '@material-symbols-react-native/outlined-400/msArrowDownward';
+import { msArrowUpward } from '@material-symbols-react-native/outlined-400/msArrowUpward';
+import { msAssignment } from '@material-symbols-react-native/outlined-400/msAssignment';
+import { msBugReport } from '@material-symbols-react-native/outlined-400/msBugReport';
+import { msClose } from '@material-symbols-react-native/outlined-400/msClose';
+import { msContentCopy } from '@material-symbols-react-native/outlined-400/msContentCopy';
+import { msDelete } from '@material-symbols-react-native/outlined-400/msDelete';
+import { msEdit } from '@material-symbols-react-native/outlined-400/msEdit';
+import { msFitnessCenter } from '@material-symbols-react-native/outlined-400/msFitnessCenter';
+import { msHistory } from '@material-symbols-react-native/outlined-400/msHistory';
+import { msInfo } from '@material-symbols-react-native/outlined-400/msInfo';
+import { msMoreHoriz } from '@material-symbols-react-native/outlined-400/msMoreHoriz';
+import { msNotes } from '@material-symbols-react-native/outlined-400/msNotes';
+import { msNotifications } from '@material-symbols-react-native/outlined-400/msNotifications';
+import { msRemove } from '@material-symbols-react-native/outlined-400/msRemove';
+import { msSave } from '@material-symbols-react-native/outlined-400/msSave';
+import { msSettings } from '@material-symbols-react-native/outlined-400/msSettings';
+import { msSettingsBackupRestore } from '@material-symbols-react-native/outlined-400/msSettingsBackupRestore';
+import { msShare } from '@material-symbols-react-native/outlined-400/msShare';
+import { msStar } from '@material-symbols-react-native/outlined-400/msStar';
+import { msTextAd } from '@material-symbols-react-native/outlined-400/msTextAd';
+import { msTranslate } from '@material-symbols-react-native/outlined-400/msTranslate';
+import { msWeight } from '@material-symbols-react-native/outlined-400/msWeight';
 import { MsIcon } from 'material-symbols-react-native';
 
-// console.log(msAdd);
+// Importing these icons using the below methods causes android app to crash
+// import { msAdd, msArrowDownward } from '@material-symbols-react-native/outlined-400';
+// import * as AllIcons from '@material-symbols-react-native/outlined-400';
 
-const x = {
-  variant: 'outlined' as const,
-  weight: 400 as const,
-  xml: '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M450-450H200v-60h250v-250h60v250h250v60H510v250h-60v-250Z"/></svg>',
+const MaterialSymbols = {
+  add: msAdd,
+  arrowDownward: msArrowDownward,
+  arrowUpward: msArrowUpward,
+  assignment: msAssignment,
+  bugReport: msBugReport,
+  close: msClose,
+  contentCopy: msContentCopy,
+  delete: msDelete,
+  edit: msEdit,
+  history: msHistory,
+  info: msInfo,
+  minus: msRemove, // Remap
+  moreHoriz: msMoreHoriz,
+  notes: msNotes,
+  notifications: msNotifications,
+  plus: msAdd, // Remap
+  remove: msRemove,
+  save: msSave,
+  settings: msSettings,
+  settingsBackupRestore: msSettingsBackupRestore,
+  share: msShare,
+  star: msStar,
+  text: msTextAd,
+  translate: msTranslate,
+  weight: msWeight,
+  fitnessCenter: msFitnessCenter,
 };
 
-import { View } from 'react-native';
-import Svg, { Circle } from 'react-native-svg';
-
-// const MaterialSymbols = {
-//   add: msAdd,
-// };
-
-// Instead of keeping the map above we can just go
-
-// import * as AllIcons from '@material-symbols-react-native/outlined-400';
-// type AllIconsType = keyof typeof AllIcons;
-// const icon = AllIcons[name]
-// This would import all the icons into the bundle though
-
-export function msIconSource(name: any) {
+export function msIconSource(name: keyof typeof MaterialSymbols) {
   return ({ size, color }: { size: number; color: string }) => {
-    return <MsIcon icon={x} size={size} color={color} />;
-    // return (
-    //   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    //     <Svg height="100" width="100">
-    //       <Circle cx="50" cy="50" r="40" fill="blue" />
-    //     </Svg>
-    //   </View>
-    // );
+    const icon = MaterialSymbols[name];
+    if (!icon) {
+      throw new Error(`Icon "${name}" not found in MaterialSymbols Map.`);
+    }
+    return <MsIcon icon={MaterialSymbols[name]} size={size} color={color} />;
   };
 }
