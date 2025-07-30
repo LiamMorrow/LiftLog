a79d6865c55b6f06087644e6ad8e3face93f1802
Liam Morrow <ldmorrow@outlook.com>
2025-07-30 16:29:53 +1000

    Add tolgee translations

--- app/services/tolgee.ts
+++ app/services/tolgee.ts
@@ -1,4 +1,13 @@
+import de from '../i18n/de.json';
 import en from '../i18n/en.json';
+import es from '../i18n/es.json';
+import fi from '../i18n/fi.json';
+import fr from '../i18n/fr.json';
+import it from '../i18n/it.json';
+import nl from '../i18n/nl.json';
+import ru from '../i18n/ru.json';
+import sr from '../i18n/sr.json';
+
 import { DevTools, FormatSimple, Tolgee } from '@tolgee/react';
 
 export const tolgee = Tolgee()
@@ -7,12 +16,20 @@ export const tolgee = Tolgee()
   .use(FormatSimple())
   // replace with .use(FormatIcu()) for rendering plurals, formatted numbers, etc.
   .init({
-    language: 'en',
-
-    // for development
+    defaultLanguage: 'en',
+    fallbackLanguage: 'en',
+    // // for development
     // apiUrl: process.env.EXPO_PUBLIC_TOLGEE_API_URL!,
     // apiKey: process.env.EXPO_PUBLIC_TOLGEE_API_KEY!,
     staticData: {
+      de,
       en,
+      es,
+      fi,
+      fr,
+      it,
+      nl,
+      ru,
+      sr,
     },
   });
