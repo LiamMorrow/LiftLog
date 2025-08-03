2326ca52daf194e75c82f0c30280e377675d162b
Liam Morrow <ldmorrow@outlook.com>
2025-08-03 12:37:31 +1000

    Add ability to edit in progress workout

--- app/components/smart/session-more-menu-component.tsx
+++ app/components/smart/session-more-menu-component.tsx
@@ -2,19 +2,19 @@ import {
   addExercise,
   selectCurrentSession,
   SessionTarget,
+  setCurrentSession,
 } from '@/store/current-session';
 import { useDispatch } from 'react-redux';
 import { useTranslate } from '@tolgee/react';
 import { useState } from 'react';
-import {
-  EmptyExerciseBlueprint,
-  ExerciseBlueprint,
-} from '@/models/session-models';
+import { ExerciseBlueprint } from '@/models/session-models';
 import FullScreenDialog from '@/components/presentation/full-screen-dialog';
 import { ExerciseEditor } from '@/components/presentation/exercise-editor';
 import { useAppSelectorWithArg } from '@/store';
 import { UnknownAction } from '@reduxjs/toolkit';
-import { Appbar, Menu } from 'react-native-paper';
+import { Appbar, Menu, TextInput } from 'react-native-paper';
+import { View } from 'react-native';
+import { spacing } from '@/hooks/useAppTheme';
 
 export default function SessionMoreMenuComponent(props: {
   target: SessionTarget;
@@ -33,6 +33,7 @@ export default function SessionMoreMenuComponent(props: {
     ExerciseBlueprint | undefined
   >(undefined);
   const [exerciseEditorOpen, setExerciseEditorOpen] = useState(false);
+  const [workoutEditorOpen, setWorkoutEditorOpen] = useState(false);
 
   const handleAddExercise = () => {
     if (editingExerciseBlueprint !== undefined) {
@@ -63,13 +64,21 @@ export default function SessionMoreMenuComponent(props: {
       >
         <Menu.Item
           onPress={() => {
-            setEditingExerciseBlueprint(EmptyExerciseBlueprint);
-            setExerciseEditorOpen(true);
+            setWorkoutEditorOpen(true);
             setMenuOpen(false);
           }}
           testID="session-add-exercise"
           leadingIcon={'add'}
           title={t('AddExercise')}
+        />
+        <Menu.Item
+          onPress={() => {
+            setWorkoutEditorOpen(true);
+            setMenuOpen(false);
+          }}
+          testID="session-edit"
+          leadingIcon={'edit'}
+          title={t('Edit workout')}
         />
       </Menu>
       <FullScreenDialog
@@ -88,6 +97,74 @@ export default function SessionMoreMenuComponent(props: {
           />
         ) : null}
       </FullScreenDialog>
+      <WorkoutEditor
+        open={workoutEditorOpen}
+        setOpen={setWorkoutEditorOpen}
+        target={props.target}
+      />
     </>
+  );
+}
+
+function WorkoutEditor(props: {
+  target: SessionTarget;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  const { t } = useTranslate();
+  const { open, setOpen, target } = props;
+  const workout = useAppSelectorWithArg(selectCurrentSession, props.target);
+  const dispatch = useDispatch();
+  const [editorWorkout, setEditorWorkout] = useState(workout);
+
+  const handleSaveWorkout = () => {
+    dispatch(setCurrentSession({ session: editorWorkout, target }));
+    setOpen(false);
+  };
+  if (!editorWorkout) {
+    return <></>;
+  }
+
+  return (
+    <FullScreenDialog
+      title={t('Edit workout')}
+      action={t('Save')}
+      open={open}
+      onAction={handleSaveWorkout}
+      onClose={() => {
+        setOpen(false);
+        setEditorWorkout(workout);
+      }}
+    >
+      <View style={{ gap: spacing[2] }}>
+        <TextInput
+          label={t('WorkoutName')}
+          testID="workout-name"
+          style={{ marginBottom: spacing[2] }}
+          value={editorWorkout.blueprint.name}
+          onChangeText={(name) =>
+            setEditorWorkout(
+              editorWorkout.with({
+                blueprint: editorWorkout.blueprint.with({ name }),
+              }),
+            )
+          }
+        />
+        <TextInput
+          label={t('PlanNotes')}
+          testID="workout-notes"
+          style={{ marginBottom: spacing[2] }}
+          value={editorWorkout.blueprint.notes}
+          onChangeText={(notes) =>
+            setEditorWorkout(
+              editorWorkout.with({
+                blueprint: editorWorkout.blueprint.with({ notes }),
+              }),
+            )
+          }
+          multiline
+        />
+      </View>
+    </FullScreenDialog>
   );
 }
