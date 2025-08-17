b2ed062288fec1ff15e12aeff3ee82ae10fe20ea
Liam Morrow <ldmorrow@outlook.com>
2025-08-17 14:48:05 +1000

    feat: Add sentry component naming at build time
    This allows sentry to show which components errors occurred in in breadcrumbs etc

--- app/metro.config.js
+++ app/metro.config.js
@@ -5,6 +5,6 @@ const {
   getSentryExpoConfig
 } = require("@sentry/react-native/metro");
 
-const config = getSentryExpoConfig(__dirname);
+const config = getSentryExpoConfig(__dirname, { annotateReactComponents: true, });
 
 module.exports = wrapWithReanimatedMetroConfig(config);
