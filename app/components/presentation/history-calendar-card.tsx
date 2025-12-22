5478855a3f2408415f916898ae750d119ab17d2e
Liam Morrow <ldmorrow@outlook.com>
2025-12-22 20:46:31 +1100

    Fix calendar card wrapping

--- app/components/presentation/history-calendar-card.tsx
+++ app/components/presentation/history-calendar-card.tsx
@@ -213,7 +213,6 @@ export default function HistoryCalendarCard({
       <Card.Content>
         <View
           style={{
-            flexDirection: 'row',
             justifyContent: 'center',
             alignItems: 'stretch',
             flex: 3,
