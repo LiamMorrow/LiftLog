2589c21738495de2595c7f80127c64adf0c33ab8
Liam Morrow <ldmorrow@outlook.com>
2026-03-02 22:15:25 +1100

    fix: Don't use dev url in prod
    A commit accidentally swapped the url used for feed/sharing api between dev and prod.

--- app/services/api-consts.ts
+++ app/services/api-consts.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-export const apiBaseUrl = !__DEV__
+export const apiBaseUrl = __DEV__
   ? Platform.OS === 'android'
     ? 'http://10.0.2.2:5264'
     : 'http://127.0.0.1:5264'
