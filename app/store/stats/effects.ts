74fdaceab5353be8407b15b0bf71f67bccf8b0c5
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2026-04-04 08:43:02 +0200

    Show average weekly sets in exercise stats

--- app/store/stats/effects.ts
+++ app/store/stats/effects.ts
@@ -223,6 +223,11 @@ function computeStats(
       unsortedStatsToWeightedStatisticOverTime(ex.statistics);
     return {
       exerciseName: ex.exerciseName,
+      setsPerWeek:
+        Object.values(ex.repsStatistics.breakdown).reduce(
+          (accum, entry) => accum + entry.numberOfSets,
+          0,
+        ) / totalWeeks,
       maxLiftedPerSessionStatistics,
       oneRepMax: ex.currentOneRepMax,
       totalVolumeStatistics: unsortedStatsToWeightedStatisticOverTime(
