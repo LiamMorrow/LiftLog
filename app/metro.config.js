43103c803e551b0461e05473fb63231eaad1a61f
Liam Morrow <ldmorrow@outlook.com>
2026-04-13 08:00:32 +1000

    fix: Add web support for sqlite

--- app/metro.config.js
+++ app/metro.config.js
@@ -7,6 +7,18 @@ const config = getSentryExpoConfig(__dirname, {
   annotateReactComponents: true,
 });
 
+// Add wasm asset support
+config.resolver.assetExts.push('wasm');
+
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    return middleware(req, res, next);
+  };
+};
+
 config.resolver.sourceExts.push('sql');
 
 module.exports = wrapWithReanimatedMetroConfig(config);
