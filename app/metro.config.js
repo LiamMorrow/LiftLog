edc8c6656fe917b879ff500436ffc4e91d0da3ec
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2026-04-12 11:25:29 +0200

    Update

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,10 +1,13 @@
 const {
   wrapWithReanimatedMetroConfig,
 } = require('react-native-reanimated/metro-config');
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-const config = getSentryExpoConfig(__dirname, { annotateReactComponents: true, });
+const config = getSentryExpoConfig(__dirname, {
+  annotateReactComponents: true,
+});
+
+config.resolver.sourceExts.push('sql');
+config.resolver.assetExts.push('wasm');
 
 module.exports = wrapWithReanimatedMetroConfig(config);
