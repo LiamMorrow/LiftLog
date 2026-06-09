7c4fc89447324cff52aac74e7176e5de7fe47cf4
Liam Morrow <ldmorrow@outlook.com>
2026-06-10 09:33:17 +1000

    fix: Enable missing languages
    This change enables the missing languages in the app that were added but not registered with our translation provider.

--- app/src/services/tolgee.ts
+++ app/src/services/tolgee.ts
@@ -2,21 +2,27 @@ import {
   DetectLanguage,
   detectLanguageOrPreferred,
 } from '@/utils/language-detector';
+
+import ar from '../i18n/ar.json';
+import cs from '../i18n/cs.json';
 import de from '../i18n/de.json';
 import en from '../i18n/en.json';
 import es from '../i18n/es.json';
 import fi from '../i18n/fi.json';
 import fr from '../i18n/fr.json';
-import it from '../i18n/it.json';
 import hu from '../i18n/hu.json';
+import it from '../i18n/it.json';
+import ko from '../i18n/ko.json';
 import nl from '../i18n/nl.json';
+import pl from '../i18n/pl.json';
+import pt from '../i18n/pt.json';
 import ru from '../i18n/ru.json';
 import sr from '../i18n/sr.json';
-import uk from '../i18n/uk.json';
-import pt from '../i18n/pt.json';
-import ar from '../i18n/ar.json';
 import sv from '../i18n/sv.json';
-import pl from '../i18n/pl.json';
+import tr from '../i18n/tr.json';
+import uk from '../i18n/uk.json';
+import zh_Hans from '../i18n/zh_Hans.json';
+
 import { FormatSimple, Tolgee } from '@tolgee/react';
 import { PreferenceService } from '@/services/preference-service';
 
@@ -36,6 +42,10 @@ export const supportedLanguages = [
   { translationJson: uk, code: 'uk', label: 'Українська' },
   { translationJson: pt, code: 'pt', label: 'Português' },
   { translationJson: pl, code: 'pl', label: 'Polski' },
+  { translationJson: cs, code: 'cs', label: 'Čeština' },
+  { translationJson: ko, code: 'ko', label: '한국어' },
+  { translationJson: tr, code: 'tr', label: 'Türkçe' },
+  { translationJson: zh_Hans, code: 'zh_Hans', label: '中文（简体）' },
 ];
 
 export const getTolgee = (preferenceService: PreferenceService) =>
